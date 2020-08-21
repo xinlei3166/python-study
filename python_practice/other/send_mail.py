eff07ef4e5343547a0ca51b8ec6adb8ac5ba657f
君惜 <xinlei3166@126.com>
2020-08-21 17:55:34 +0800

    Update send_mail.py
    remove sensitive info.

--- python_practice/other/send_mail.py
+++ python_practice/other/send_mail.py
@@ -7,12 +7,12 @@ from email.mime.text import MIMEText
 
 # msg = MIMEText('send by python...', 'plain', 'utf-8')
 msg = MIMEText('<html><body><h1>Hello</h1>' + '<p>send by <a href="http://www.xuegod-for.cn/yum">python</a></body></html>', 'html', 'utf-8')
-msg['From'] = "xiaoxinsoso@qq.com"
-msg["To"] = "xinlei3166@126.com"
+msg['From'] = "xiaoxin@qq.com"
+msg["To"] = "xinlei@126.com"
 msg["Subject"] = "python test"
 
 server = smtplib.SMTP_SSL('smtp.qq.com', 465)
 server.set_debuglevel(1)
-server.login("xiaoxinsoso@qq.com", "xejhmwedtjrubihb")
-server.sendmail("xiaoxinsoso@qq.com",["xinlei3166@126.com"],msg.as_string())
+server.login("xiaoxin@qq.com", "xxxxxx")
+server.sendmail("xiaoxin@qq.com",["xinlei@126.com"],msg.as_string())
 server.quit()
